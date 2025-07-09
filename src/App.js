392927f3f8afd59cde509ba83559e395524a0d6c
saniya <saniyagarg9@gmail.com>
2025-07-09 15:12:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { getDatabase ,ref,set} from 'firebase/database';
+import {app} from './firebase'
 import './App.css';
-
+//user/saniya is the name of the root where we have to put the data
+const db=getDatabase(app);
 function App() {
+  const putData=()=>
+  {
+    set(ref(db,'users/saniya' ),{
+      id:1,
+      name:"saniya",
+      age:19,
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My firebase app</h1>
+      <button className="button" onClick={putData}>Put data</button>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+//to connect the firebase app to the firebase account 
+const firebaseConfig = {
+  apiKey: "AIzaSyCg0Bcay1lkx18u66f69HQzc4QKqUFWCng",
+  authDomain: "app-5bd05.firebaseapp.com",
+  projectId: "app-5bd05",
+  storageBucket: "app-5bd05.firebasestorage.app",
+  messagingSenderId: "319887334854",
+  appId: "1:319887334854:web:15820958115e61074866c7",
+  measurementId: "G-5CK1B7JY8W",
+  databaseURL:"https://app-5bd05-default-rtdb.firebaseio.com",
+
+
+
+};
+export const app=initializeApp(firebaseConfig);
