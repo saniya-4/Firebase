ab955faa758575aadc48612036e9c94e1b7a61ea
saniya <saniyagarg9@gmail.com>
2025-07-11 02:19:40 +0530

    added the features of signout and to find if there is a user logged in or not

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { getDatabase ,ref,set} from 'firebase/database';
-import {getAuth,createUserWithEmailAndPassword} from 'firebase/auth'
+import {getAuth,createUserWithEmailAndPassword,onAuthStateChanged,signOut} from 'firebase/auth'
+import {useEffect,useState} from "react";
 import SignUpPage from './Components/pages';
 import SignInPage from './Components/SignIn'
 import {app} from './firebase'
@@ -8,10 +9,42 @@ import './App.css';
 const db=getDatabase(app);
 const auth=getAuth(app);
 function App() {
-  const signUp=()=>
+  const [user,setUser]=useState(null);
+  useEffect(() => {
+    
+    onAuthStateChanged(auth, (user) => {
+      if(user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+  if(user===null)
   {
-    createUserWithEmailAndPassword(auth,'saniyagarg@gmail.com','saniya123').then((value)=>console.log(value));
+    return (
+    <div className="App">
+      <SignUpPage/>
+      <SignInPage/>
+    </div>
+  );
   }
+  const signUp = () => {
+    createUserWithEmailAndPassword(auth, 'saniyagarg@gmail.com', 'saniya123')
+      .then((userCredential) => {
+        const user = userCredential.user;
+        // Write user data to database
+        set(ref(db, 'users/' + user.uid), {
+          email: user.email,
+          // add other fields as needed
+        });
+        console.log('User signed up and data written to database');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   const putData=()=>
   {
     set(ref(db,'users/saniya' ),{
@@ -20,12 +53,14 @@ function App() {
       age:19,
     })
   }
-  return (
+  return(
     <div className="App">
-      <SignUpPage/>
-      <SignInPage/>
+      <h1>Hello {user.email}</h1>
+      <button onClick={()=>signOut(auth)}>SignOut</button>
     </div>
-  );
+
+  )
+  
 }
 
 export default App;
