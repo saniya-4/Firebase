babedeee87e42fc1737e4fcd3d5a328d04b88ba1
saniya <saniyagarg9@gmail.com>
2025-07-10 23:22:45 +0530

    Added how to use the google authentication with firebase

--- src/Components/pages.jsx
+++ src/Components/pages.jsx
@@ -1,11 +1,18 @@
 import React,{useState} from 'react'
-import {getAuth,createUserWithEmailAndPassword} from 'firebase/auth'
+import {getAuth,createUserWithEmailAndPassword,GoogleAuthProvider,
+    signInWithPopup
+} from 'firebase/auth'
 import {app} from '../firebase'
 const SignUpPage=()=>
 {
+    const googleProvider= new GoogleAuthProvider();
     const auth=getAuth(app);
     const [email,setEmail]=useState("");
     const [passwd,setPasswd]=useState("");
+    const signupWithGoogle=()=>
+    {
+        signInWithPopup(auth,googleProvider);
+    }
     const createUser=()=>
     {
         createUserWithEmailAndPassword(
@@ -20,6 +27,8 @@ const SignUpPage=()=>
             <input  onChange={(e)=>setEmail(e.target.value)} value={email} type="email" required placeholder="Enter your email"/>
             <label>Password</label>
             <input  onChange={(e)=>setPasswd(e.target.value)} value={passwd}  type="Password" required placeholder="Enter your password"/>
+            <br/>
+            <button onClick={signupWithGoogle}>Sign In with Google</button>
             <button onClick={createUser}>SignUp</button>
         </div>
     )
